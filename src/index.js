cfd081397b50541e1caf18e90645a16a1807078c
Yi <zhyi.feynman@gmail.com>
2023-02-17 12:43:11 +0800

    continue

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@
 //测试 git commit -am
 //Test git diff
 <<<<<<< HEAD
+<<<<<<< HEAD
 //修改branch1
 //rebase 3
 //rebase 4
 =======
 //修改main
 >>>>>>> d20c294 (git rebase branch1)
+=======
+//修改main
+=======
+//测试main分支与branch1分支开叉
+//测试修改main
+>>>>>>> 2a980d2 (修改main)
+>>>>>>> bfb9445 (git rebase --continue)
