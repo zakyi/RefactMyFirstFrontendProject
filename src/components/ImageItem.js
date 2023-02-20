558887194ae0ae70a2dccd9865a903e22e658e8e
Yi <zhyi.feynman@gmail.com>
2023-02-20 13:07:29 +0800

    图片添加overlay

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -1,5 +1,25 @@
+import "./ImageItem.css";
+import { useState } from "react";
+import Button from "./Button";
+
 function ImageItem({ image }) {
-  return <img src={image.path} />;
+  const [isOnMouse, setIsOnMouse] = useState(false);
+
+  const handleOnMouseOver = () => {};
+
+  return (
+    <div className="image-container">
+      <img className="image" onMouseOver={handleOnMouseOver} src={image.path} />
+      <div className="image-overlay">
+        <Button type="secondary" decoration="rounded">
+          + Like
+        </Button>
+        <Button type="secondary" decoration="rounded">
+          + Add
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default ImageItem;
