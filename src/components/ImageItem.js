6c9d0554a5e47e7fa9bce03013867ca3976aac9e
Yi <zhyi.feynman@gmail.com>
2023-02-23 18:59:30 +0800

    IntersectionObserver实现图片懒加载

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -2,6 +2,8 @@ import "./ImageItem.css";
 import Button from "./Button";
 import { useSelector } from "react-redux";
 import Link from "./Link";
+import { useState, useRef } from "react";
+import { useHelperHook } from "../hooks/useHelperHook";
 
 function ImageItem({
   image,
@@ -12,6 +14,9 @@ function ImageItem({
   onOpenModal,
 }) {
   const { isLoggedIn } = useSelector((state) => state.userData);
+  const [isInView, setIsInview] = useState(false);
+  const imgRef = useRef();
+  const { useIntersection } = useHelperHook();
 
   const checkLiked = () => {
     return likes.includes(image.id) ? "liked" : "";
@@ -21,32 +26,54 @@ function ImageItem({
     return collections.includes(image.id) ? "added" : "";
   };
 
-  return (
-    <div className="image-container">
-      <img
-        data-image-id={image.id}
-        data-image-path={image.path}
-        className="image"
-        src={image.path}
-        alt="img"
-      />
+  console.log(imgRef);
+  useIntersection(imgRef, () => {
+    setIsInview(true);
+  });
 
-      <div className="image-overlay" onClick={onOpenModal}>
-        <Button handleClick={handleLike} type="secondary" decoration="rounded">
-          {isLoggedIn ? (
-            <p className={checkLiked()}>❤ Like</p>
-          ) : (
-            <Link classes="login-link" label="❤ Like" path="/login" />
-          )}
-        </Button>
-        <Button handleClick={handleAdd} type="secondary" decoration="rounded">
-          {isLoggedIn ? (
-            <p className={checkAdded()}>+ Add</p>
-          ) : (
-            <Link classes="login-link" label="+ Add" path="/login" />
-          )}
-        </Button>
-      </div>
+  /**
+   * 懒加载相关，图片不在视野内就移除背景padding
+   */
+  const containerStyle = isInView
+    ? {}
+    : { paddingBottom: `${+((image.height / image.width) * 100)}%` };
+  return (
+    <div ref={imgRef} className="image-container" style={containerStyle}>
+      {isInView && (
+        <div>
+          <img
+            data-image-id={image.id}
+            data-image-path={image.path}
+            className="image"
+            src={image.path}
+            alt="img"
+          />
+          <div className="image-overlay" onClick={onOpenModal}>
+            <Button
+              handleClick={handleLike}
+              type="secondary"
+              decoration="rounded"
+            >
+              {isLoggedIn ? (
+                <p className={checkLiked()}>❤ Like</p>
+              ) : (
+                <Link classes="login-link" label="❤ Like" path="/login" />
+              )}
+            </Button>
+            <Button
+              handleClick={handleAdd}
+              type="secondary"
+              decoration="rounded"
+            >
+              {isLoggedIn ? (
+                <p className={checkAdded()}>+ Add</p>
+              ) : (
+                <Link classes="login-link" label="+ Add" path="/login" />
+              )}
+            </Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/hooks/useHelperHook.js
+++ src/hooks/useHelperHook.js
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { setSearchTimeout, getSearchState } from "../store";
+import { useEffect } from "react";
 
 function useHelperHook() {
   const { searchTimeout } = useSelector((state) => state.search);
@@ -20,7 +21,53 @@ function useHelperHook() {
     };
   }
 
-  return { debouce };
+  let observer;
+
+  let observedTargets = new WeakMap();
+
+  function useIntersection(ref, handle) {
+    const handleIntersection = (allDOMs) => {
+      //entries是所有DOM元素
+      allDOMs.forEach((currentDOM) => {
+        const currentDomIsIntersectingViewPort =
+          currentDOM.isIntersecting || currentDOM.intersectionRatio > 0;
+
+        const currentDomIsObserved = observedTargets.has(currentDOM.target);
+
+        if (currentDomIsIntersectingViewPort && currentDomIsObserved) {
+          const handle = observedTargets.get(currentDOM.target);
+          handle();
+          observer.unobserve(currentDOM.target);
+          observedTargets.delete(currentDOM.target);
+        }
+      });
+    };
+
+    const option = {
+      root: null,
+      rootMargin: "0%",
+      threshold: "0.1",
+    };
+
+    //只在第一次渲染页面的时候向所有图片插入观察器
+    useEffect(() => {
+      const target = ref.current;
+      console.log(ref);
+      if (!observer) {
+        observer = new IntersectionObserver(handleIntersection, option);
+      }
+      observedTargets.set(target, handle);
+      observer.observe(target);
+      //下一次渲染的时候清除
+      return () => {
+        //无论观没观察到target，都清除掉
+        observedTargets.delete(target);
+        observer.unobserve(target);
+      };
+    }, []);
+  }
+
+  return { debouce, useIntersection };
 }
 
 export { useHelperHook };
