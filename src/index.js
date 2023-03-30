4bb67563d92f2f8f40b18457decff5b8403c9590
Yi <zhyi.feynman@gmail.com>
2023-03-30 19:17:41 +0800

    改变ROOT_URL

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,6 +14,7 @@ root.render(
   </Provider>
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/static/config.js
+++ src/static/config.js
@@ -1,2 +1,2 @@
-export const ROOT_URL = "http://localhost";
+export const ROOT_URL = "http://10.28.227.147";
 export const DEBOUNCE_DELAY = 1000;
