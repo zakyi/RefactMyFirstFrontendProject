91b6128227618276e43b29169f3ca352004a1c6a
Yi <zhyi.feynman@gmail.com>
2023-03-31 13:38:20 +0800

    test SW

--- src/service-worker.js
+++ src/service-worker.js
@@ -71,3 +71,6 @@ self.addEventListener("message", (event) => {
 });
 
 // Any other custom service worker logic can go here.
+self.addEventListener("fetch", function (event) {
+  console.log("fetch");
+});
