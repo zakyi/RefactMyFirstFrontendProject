ec15fb57e585c4becb459f1bcbda81bbe0a567b4
Yi <zhyi.feynman@gmail.com>
2023-04-04 17:24:12 +0800

    修改nginx端口

--- src/static/config.js
+++ src/static/config.js
@@ -1,2 +1,2 @@
-export const ROOT_URL = "https://[2001:da8:215:3c02:ac9e:f2f:d82b:d4f9]";
+export const ROOT_URL = "https://[2001:da8:215:3c02:3a94:4a6a:6724:cd]";
 export const DEBOUNCE_DELAY = 1000;
